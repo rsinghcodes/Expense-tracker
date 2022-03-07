cfc660018a2570909fe1e554b860f493a29f7a55
Raghvendra Singh <raghvendrrsingh@gmail.com>
2022-03-07 18:14:13 +0530

    Deployment issue fixed

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ if (NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 
   app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'))
   );
 }
 
