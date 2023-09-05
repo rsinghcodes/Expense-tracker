ecd675fb496cfa32a0168ab16d35e7a3a5f57ab8
Raghvendra Singh <raghvendrrsingh@gmail.com>
2023-09-06 00:31:49 +0530

    Updated frontend packages and fixed backend issues

--- server/src/database/connect.js
+++ server/src/database/connect.js
@@ -3,12 +3,11 @@ import mongoose from 'mongoose';
 async function connect({ db }) {
   try {
     await mongoose
+      .set('strictQuery', false)
       .connect(db)
-      .then(() =>
-        console.log(`Successfully connected to Database`.cyan.underline.bold)
-      );
+      .then(() => console.log('Successfully connected to Database'));
   } catch (error) {
-    console.log(`An error ocurred when trying to connect with Database`.red);
+    console.log('An error ocurred when trying to connect with Database');
     throw error;
   }
 }
--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import morgan from 'morgan';
-import path from 'path';
 
 import { MONOGO_DB, NODE_ENV, PORT } from './config';
 import connect from './database/connect';
@@ -16,15 +15,7 @@ if (NODE_ENV === 'development') {
 
 app.use('/api/transactions', transactions);
 
-if (NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'))
-  );
-}
-
 app.listen(PORT, async () => {
-  console.log(`Server running on ${NODE_ENV} mode on port ${PORT}`.yellow.bold);
+  console.log(`Server running on ${NODE_ENV} mode on port ${PORT}`);
   await connect({ db: MONOGO_DB });
 });
